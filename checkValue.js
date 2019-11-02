d37566b40dfc53ec88b45c1e4d92e485efd23dfc
Jackie <JGtheophila19@gmail.com>
2019-11-01 23:52:29 -0400

    Use the Console to Check the Value of a Variable

---
+++ checkValue.js
@@ -0,0 +1,20 @@
+let a = 5;
+
+let b = 1;
+
+a++;
+
+console.log(a);
+// use console.log() to check the value of the variable a
+
+let sumAB = a + b;
+console.log(sumAB);
+
+/* Note: JavaScript consoles is known as DevTools, for debugging JavaScript.
+
+The console.log() method, which "prints" the output of what is within its 
+parentheses to the console, will likely be the most helpful debugging tool. 
+
+Placing it at strategic points in your code can show the intermediate values of variables. 
+It gives an idea of what the output should be before looking at what it is. 
+Having check points throughout your code will help narrow down where the problem is.*/
