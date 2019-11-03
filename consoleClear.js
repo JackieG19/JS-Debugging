ccf4e201398fd882bdcb54a728d7f06263d5aef2
Jackie <JGtheophila19@gmail.com>
2019-11-02 23:53:08 -0400

    using console.clear() to clear the browser console

---
+++ consoleClear.js
@@ -0,0 +1,8 @@
+let output = "Get this to log once in the browser console and twice in the freeCodeCamp console";
+
+// console.clear() to clear the browser console.
+console.clear();
+
+// console.log() to print the output variable.
+console.log(output);
+
