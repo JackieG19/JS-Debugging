cb4dd762709b4ccd07cf5f4cda9f82b56ff6359a
Jackie <JGtheophila19@gmail.com>
2019-11-09 21:40:54 -0500

    use typeof to debugging with multiple data types

---
+++ typeof.js
@@ -0,0 +1,10 @@
+/* use typeof to check the data structure, or type, of a variable. 
+This is useful in debugging when working with multiple data types.*/
+
+let seven = 7;
+let three = "3";
+
+console.log(seven + three);
+
+console.log(typeof seven);
+console.log(typeof three);
